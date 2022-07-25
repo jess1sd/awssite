b8ec0d7de5ba1e6d02e9e84ec27ba153974b28d1
jchiang <jessica.chiang@sony.com>
2022-07-25 08:39:19 -0700

    test update

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations Jess! </h1>" +
+return "<h1> Congratulations WV MOM ! </h1>" +
         "<p>You have successfully deployed the sample Spring Boot Application. </p>";
 }
 }
